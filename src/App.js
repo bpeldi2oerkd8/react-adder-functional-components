8c16440915ce6b7b212746cf9e19317afbb9a635
bpeldi2oerkd8 <takahashi208pwla4drb@gmail.com>
2022-05-04 11:59:55 +0900

    加算器の機能追加

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Adder from './components/Adder';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>
+          Adder
+        </h1>
       </header>
+      <main>
+        <Adder />
+      </main>
     </div>
   );
 }
---
+++ src/components/Adder.jsx
@@ -0,0 +1,40 @@
+import React, {useState} from 'react';
+
+const Adder = () => {
+  const [sum, setSum] = useState(0);
+  const [increase, setIncrease] = useState('1');
+  const add = () => {
+    const parsed = parseInt(increase);
+    if (isNaN(parsed)) {
+      alert('増加量には数字を入力してください');
+    } else if (parsed < 0) {
+      alert('増加量には0以上の値を入力してください');
+    }else {
+      setSum(sum + parsed);
+    }
+  };
+  const reset = () => {
+    setSum(0);
+  };
+  const changeIncrease = (e) => {
+    const value = e.target.value;
+    const parsed = parseInt(value);
+    // 空文字か0以上の数字のみ入力可
+    if (value === '' || (!isNaN(parsed) && parsed >= 0)) {
+      setIncrease(value);
+    } else {
+      alert('0以上の数字を入力してください');
+    }
+  };
+
+  return (
+    <div className="counter">
+      <h2>合計：{sum}</h2>
+      <p>増加量：<input type="number" value={increase} onChange={changeIncrease}/></p>
+      <button onClick={add}>足す</button>
+      <button onClick={reset}>リセット</button>
+    </div>
+  );
+};
+
+export default Adder;
